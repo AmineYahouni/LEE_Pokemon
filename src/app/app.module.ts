cbdc3ad65243b5433c64df61ab0a8a4fab5852d4
amineyahouni <amineyahouni@gmail.com>
2020-12-10 00:31:03 +0100

    retrait des component et ajout du module PokemonsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListPokemonComponent } from './pokemons/list-pokemon/list-pokemon.component';
 import {FormsModule} from '@angular/forms';
 import { BorderCardDirective } from './shared/directives/border-card.directive';
 import { PokemonTypeColorPipe } from './shared/pipes/pokemon-type-color.pipe';
-import { DetailPokemonComponent } from './pokemons/detail-pokemon/detail-pokemon.component';
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import {PokemonsModule} from './pokemons/pokemons.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListPokemonComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    DetailPokemonComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    PokemonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
