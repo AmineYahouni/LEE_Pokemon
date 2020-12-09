6bba242345378f747427eb6555163ddf1b4034c3
amineyahouni <amineyahouni@gmail.com>
2020-12-10 00:51:15 +0100

    Création des méthodes getListPokemon et getSinglePokemon

--- src/app/pokemons/pokemons.service.ts
+++ src/app/pokemons/pokemons.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import {Pokemon} from '../pokemon';
+import {POKEMONS} from '../shared/list.pokemons';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,18 @@ import { Injectable } from '@angular/core';
 export class PokemonsService {
 
   constructor() { }
+
+  getListPokemons(): Pokemon[] {
+    return POKEMONS;
+  }
+
+  getSinglePokemon(id: number): Pokemon {
+    const listPkm = this.getListPokemons();
+
+    for (let i = 0; i < listPkm.length; i++) {
+      if (id === listPkm[i].id)  {
+        return listPkm[i];
+      }
+    }
+  }
 }
