88e8baa915b4c1a0173050e8a5dfe0e6f4566866
amineyahouni <amineyahouni@gmail.com>
2020-12-10 00:50:36 +0100

    Remplacement de la boucle par la méthode de notre service

--- src/app/pokemons/detail-pokemon/detail-pokemon.component.ts
+++ src/app/pokemons/detail-pokemon/detail-pokemon.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Pokemon} from '../../pokemon';
 import {POKEMONS} from '../../shared/list.pokemons';
+import {PokemonsService} from '../pokemons.service';
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -10,19 +11,20 @@ import {POKEMONS} from '../../shared/list.pokemons';
 })
 export class DetailPokemonComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private router: Router) {
-    this.listOfPokemons = POKEMONS;
+  constructor(private route: ActivatedRoute, private router: Router, private pokemonsService: PokemonsService) {
+    //this.listOfPokemons = this.pokemonsService.getListPokemons();
   }
 
-  listOfPokemons: Pokemon[] = null;
+  //listOfPokemons: Pokemon[] = null;
   pokemonToDisplay: Pokemon = null;
   ngOnInit(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    for (let i = 0; i < this.listOfPokemons.length; i++){
+   /* for (let i = 0; i < this.listOfPokemons.length; i++){
       if (this.listOfPokemons[i].id === id) {
         this.pokemonToDisplay = this.listOfPokemons[i];
       }
-    }
+    }*/
+  this.pokemonToDisplay = this.pokemonsService.getSinglePokemon(id);
     console.log(this.pokemonToDisplay);
   }
 
