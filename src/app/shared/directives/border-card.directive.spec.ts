b0f93e9fa4da25f7264a8eb5e84e395c62ce071c
amineyahouni <amineyahouni@gmail.com>
2020-12-10 04:02:19 +0100

    final

--- src/app/shared/directives/border-card.directive.spec.ts
+++ src/app/shared/directives/border-card.directive.spec.ts
@@ -2,6 +2,7 @@ import { BorderCardDirective } from './border-card.directive';
 
 describe('BorderCardDirective', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     const directive = new BorderCardDirective();
     expect(directive).toBeTruthy();
   });
